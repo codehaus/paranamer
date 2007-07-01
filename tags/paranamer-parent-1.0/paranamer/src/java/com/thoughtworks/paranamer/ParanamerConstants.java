46372b69dbf025e674b5725d132e145232ce5edb
paul <paul@75d258eb-f418-0410-b93b-c6a5b200b6a1>
2007-07-01 16:51:14 +0000

    [maven-release-plugin]  copy for tag paranamer-parent-1.0

---
+++ tags/paranamer-parent-1.0/paranamer/src/java/com/thoughtworks/paranamer/ParanamerConstants.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.paranamer;
+
+/**
+ * Constants shared by all Paranamer components
+ * 
+ * @author Mauro Talevi
+ */
+public final class ParanamerConstants {
+
+    public static final String HEADER = "v1.0";
+    
+}
